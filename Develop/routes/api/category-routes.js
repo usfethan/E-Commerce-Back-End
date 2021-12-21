296cd4f0d3242cbdf4b7c8e16ccc47cd8bc6dca8
Fethan Us <usfethan@gmail.com>
2021-12-21 12:14:14 -0500

    created route for creating new category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -35,6 +35,12 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
