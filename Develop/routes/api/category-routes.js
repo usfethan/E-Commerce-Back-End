0ae02b16f2e2e6956d20acd7f2c5e971ad75f24a
Fethan Us <usfethan@gmail.com>
2021-12-21 12:05:11 -0500

    created get all categories route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,6 +6,14 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
