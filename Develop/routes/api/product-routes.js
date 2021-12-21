db7c15403ba9e29e4034cdf908784ff4926de0c7
Fethan Us <usfethan@gmail.com>
2021-12-21 12:34:27 -0500

    created get one product by id and check is it include or not

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -21,6 +21,18 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+    if(!productData) {
+      res.status(404).json({ message: "There is no tag with this id!!"});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
