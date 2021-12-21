d0faf0a243f8347544cb4748d106b3f389d6e67f
Fethan Us <usfethan@gmail.com>
2021-12-21 12:46:38 -0500

    created delete one product by id

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -111,6 +111,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      },
+    });
+    if(!productData) {
+      res.status(404).json({ message: "There is no category found with this id!!"});
+      return;
+
+    }
+    res.status(200).json(productData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
