eb662d74254f8b732ba931b84a566b58dc28dd63
Fethan Us <usfethan@gmail.com>
2021-12-21 12:56:43 -0500

    created put request for update a tag's name by id

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -45,6 +45,20 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if(!tagData) {
+      res.status(404).json({ message: "There is no tag found with this id!!"});
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
