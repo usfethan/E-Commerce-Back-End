b2fffb910a178d65824d3896e5a2f55c5e09fcf2
Fethan Us <usfethan@gmail.com>
2021-12-21 12:10:47 -0500

    created route for find one category by id

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -19,6 +19,18 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if(!categoryData) {
+      res.status(404).json({ message: "There is no category with this id!!"});
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
